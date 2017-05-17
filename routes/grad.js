daf5b7bcaef0beff51a0c9dfdc04b54bb9c491b6
gapoorva <gapoorva@umich.edu>
2017-05-17 09:10:47 -0400

    adding assets and css for timeline

--- routes/grad.js
+++ routes/grad.js
@@ -3,8 +3,19 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
   //res.send('respond with a resource');
-
-  res.render('index', {'title': 'Riya\'s Graduation'})
+  var context = {
+    'title': 'Congrats Riya!',
+    'timeline': true,
+    'events' : [
+      {
+        'eventTitle': 'Riya\'s Grad Party',
+        'eventImage': 'assets/events/event-00.jpg',
+        'eventDescription': '<p>Riya\'s parents, brother, dog, and family all helped to host a grad party for her at <a href="">Rochester Community House</a>. To celebrate her accomplishments, gathered to congratulate the new graduate and share some good food. Some of her family even flew across the world to be there.</p><p>We all wish you the very best luck as you continue your journey onwards towards new adventures at the University of Michigan. Go Blue!</p>' ,
+        'eventDateDescription': 'Sunday, May 28th'
+      }
+    ]
+  }
+  res.render('grad', context);
 });
 
 module.exports = router;
